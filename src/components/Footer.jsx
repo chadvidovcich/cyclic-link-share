32a56d4dddb940039bf79c3821f8930de1ac3019
Chad Vidovcich <97252914+chadvidovcich@users.noreply.github.com>
2023-03-19 17:16:46 -0700

    install tailwind

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,6 +7,8 @@ function Footer() {
         <div className="md:w-4/6 mx-auto">
           <p className="py-4 text-center text-xs sm:text-sm font-light text-white">
             Thanks for visiting!
+            <br />
+            Deployed on <a href="https://www.cyclic.sh/">Cyclic</a>
           </p>
         </div>
       </div>
